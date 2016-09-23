e74339b62870e480abb45ff97ca2123db7c5cbd8
salilkansal <sxk150430@utdallas.edu>
2016-09-23 11:59:14 -0500

    Project working with TCP

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -5,36 +5,32 @@
 netid=sxk150430
 
 #
-# Root directory of your project
-PROJDIR=$HOME/Desktop/ProjectAOS
-
-#
 # This assumes your config file is named "config.txt"
 # and is located in your project directory
 #
-CONFIG=$PROJDIR/config.txt
+CONFIG=configuration/configuration.txt
 
 #
 # Directory your java classes are in
 #
-BINDIR=$PROJDIR/bin
+BINDIR=AOSProject/bin
 
 #
 # Your main project class
 #
-PROG=Project1
+PROG=Project
 
 
 cat ${CONFIG} | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
 (
     read i
     END=${i}
-	for ((i=0;i<END;i++)); 
+	for ((i=0;i<END;i++));
 	do
     	read line
     	host=$( echo $line | awk '{ print $2 }' )
         port=$( echo $line | awk '{ print $3 }' )
-        echo $netid@$host java $BINDIR/$PROG $i $port
+        ssh $netid@$host java $BINDIR/$PROG $i $port &
 	done
    
 )
